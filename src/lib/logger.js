971505f6a6dfb4e52b9cb4318f55789aa13fc979
Maze Peterson <mazecodes@protonmail.com>
2021-02-06 00:03:14 -0800

    Add koala logger

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -39,8 +39,22 @@ const warning = message => {
   console.log(`${chalk.yellowBright('[WARNING]')} ${message}`);
 };
 
+/**
+ * Log a koala message
+ *
+ * @param {String} message - Log message
+ * @returns {void}
+ *
+ * @example
+ *   koala('Message')
+ */
+const koala = message => {
+  console.log(`${chalk.greenBright('[KOALA]')} ${message}`);
+};
+
 module.exports = {
   success,
   error,
   warning,
+  koala,
 };
